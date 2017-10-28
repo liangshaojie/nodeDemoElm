2250bbd64d6ffa7f705d8db5766640442972a818
877942720@qq.com <lsj890305>
2017-10-28 13:23:35 +0800

    chu

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 /**
  * Created by Administrator on 2017/10/28/028.
  */
+import express from 'express';
+const app = express();
+
+app.listen(1234,function(){
+    console.log("成功监听在1234端口")
+})
+
+
