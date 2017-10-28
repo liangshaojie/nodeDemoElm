240cfe8b9617681e8616d0682e08239cf76a121c
877942720@qq.com <lsj890305>
2017-10-28 14:14:03 +0800

    成功添加session

--- routes/v1.js
+++ routes/v1.js
@@ -3,6 +3,8 @@ import express from 'express'
 const router = express.Router();
 
 router.get('/cities', function(req, res, next){
+    req.session.login = "1";
+    req.session.username = "lisangshojei";
     res.send("哈哈哈哈")
 });
 
