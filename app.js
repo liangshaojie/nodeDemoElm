6940b4a189b670bbc3ea191b534761370ca7edc4
877942720@qq.com <lsj890305>
2017-10-28 14:04:03 +0800

    添加配置文件成功

--- app.js
+++ app.js
@@ -3,13 +3,14 @@
  */
 import express from 'express';
 import router from './routes/index.js';
+//88888888888888888
+var config = require('config-lite')(__dirname);
 const app = express();
 
 router(app);
 
-
-app.listen(1234,function(){
-    console.log("成功监听在1234端口")
+app.listen(config.port,function(){
+    console.log("成功监听在"+config.port+"端口")
 })
 
 
