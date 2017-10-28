48420240a1f7e40e83e8ae60a0b913bea2e38e4c
877942720@qq.com <lsj890305>
2017-10-28 20:01:48 +0800

    开启热更新，

--- controller/v1/cities.js
+++ controller/v1/cities.js
@@ -1,9 +1,11 @@
 'use strict';
 import Cities from '../../models/v1/cities'
+import pinyin from "pinyin"
 import AddressComponent from '../../prototype/addressComponent'
 class CityHandle extends AddressComponent{
     constructor(){
-        super()
+		super()
+		this.getCity = this.getCity.bind(this);
     }
     async getCity(req, res, next){
         const type = req.query.type;
@@ -11,18 +13,17 @@ class CityHandle extends AddressComponent{
         try{
 			switch (type){
 				case 'guess': 
-					// const city = await this.getCityName(req);
-                    // cityInfo = await Cities.cityGuess(city);
-                    cityInfo = 'guess'
+					const city = await this.getCityName(req);
+					cityInfo = await Cities.cityGuess(city);
 					break;
 				case 'hot': 
                     cityInfo = await Cities.cityHot();
 					break;
 				case 'group': 
-                    // cityInfo = await Cities.cityGroup();
-                    cityInfo = 'group'
+                    cityInfo = await Cities.cityGroup();
 					break;
 				default: 
+					console.log(6666666);
 					res.json({
 						name: 'ERROR_QUERY_TYPE',
 						message: '参数错误',
@@ -36,7 +37,31 @@ class CityHandle extends AddressComponent{
 				message: '获取数据失败',
 			});
 		}
-    }
+	}
+	async getCityName(req){
+		let cityInfo;
+		try{
+			cityInfo = await this.guessPosition(req);
+		}catch(err){
+			console.error('获取IP位置信息失败', err);
+			res.send({
+				name: 'ERROR_DATA',
+				message: '获取数据失败',
+			});
+			return 
+		}
+		/*
+		汉字转换成拼音
+		 */
+        const pinyinArr = pinyin(cityInfo.city, {
+		  	style: pinyin.STYLE_NORMAL,
+		});
+		let cityName = '';
+		pinyinArr.forEach(item => {
+			cityName += item[0];
+		})
+		return cityName
+	}
 }
 
 export default new CityHandle()
--- models/v1/cities.js
+++ models/v1/cities.js
@@ -10,8 +10,51 @@ const citySchema = new mongoose.Schema({
 citySchema.statics.cityHot = function (){
 	return new Promise(async (resolve, reject) => {
 		try{
+			//操作数据库是异步操作，所以用async await
 			const city = await this.findOne();
 			resolve(city.data.hotCities)
+		}catch(err){
+			reject({
+				name: 'ERROR_DATA',
+				message: '查找数据失败',
+			});
+			console.error(err);
+		}
+	})
+}
+
+citySchema.statics.cityGroup = function (){
+	return new Promise(async (resolve, reject) => {
+		try{
+			const city = await this.findOne();
+			const cityObj = city.data;
+			//删除对象的一个键值对（key:value）
+			delete(cityObj.hotCities)
+			resolve(cityObj)
+		}catch(err){
+			reject({
+				name: 'ERROR_DATA',
+				message: '查找数据失败',
+			});
+			console.error(err);
+		}
+	})
+}
+
+citySchema.statics.cityGuess = function(name){
+	return new Promise(async (resolve, reject) => {
+		const firtWord = name.substr(0,1).toUpperCase();
+		try{
+			const city = await this.findOne();
+			Object.entries(city.data).forEach(item => {
+				if(item[0] == firtWord){
+					item[1].forEach(cityItem => {
+						if (cityItem.pinyin == name) {
+							resolve(cityItem)
+						}
+					})
+				}
+			})
 		}catch(err){
 			reject({
 				name: 'ERROR_DATA',
@@ -26,6 +69,7 @@ citySchema.statics.cityHot = function (){
 const Cities = mongoose.model('Cities', citySchema);
 
 Cities.findOne((err, data) => {
+	//第一次进入如果数据库没有数据，那么就去插入数据，根据本地文件
 	if (!data) {
 		Cities.create({data: cityData});
 	}
